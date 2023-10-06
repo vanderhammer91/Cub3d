21c73f254973e83176c651dd1fba4d6975c00cdb
Isaac Vanderwal <isaacvanderwal@hotmail.com>
2023-10-06 13:55:13 +1030

    norm: box_collider

--- src/box_collider.c
+++ src/box_collider.c
@@ -6,20 +6,73 @@
 /*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:27:34 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/10/06 10:35:35 by ivanderw         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:54:49 by ivanderw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "box_collider.h"
 
-void	c3d_collider_check_all(t_game *game)
-{	
-	t_ray	this_ray;
-	int 	has_set;
-	int	j;
-	int	i;
+void	c3d_collider_check_all_do1(t_bound *this_wall,
+		t_ray *this_ray, int *has_set)
+{
+	t_point	*point;
 	float	pt_dist;
-	t_point *point;
+
+	point = NULL;
+	point = ray_cast(this_wall, this_ray);
+	if (point && this_wall->type != DOOR)
+	{
+		pt_dist = -1;
+		pt_dist = get_raylength(*this_ray, *point);
+		if (pt_dist != -1 && pt_dist < 15)
+			*has_set = 1;
+	}
+	else if (point && this_wall->type == DOOR)
+	{
+		if (this_wall->is_active)
+		{
+			pt_dist = -1;
+			pt_dist = get_raylength(*this_ray, *point);
+			if (pt_dist != -1 && pt_dist < 15)
+				*has_set = 1;
+		}
+	}
+	if (point)
+		free(point);
+}
+
+void	c3d_set_bumpers(t_game *game, t_ray this_ray, int has_set)
+{
+	if (has_set)
+	{
+		if (this_ray.rot == 0)
+			game->player.rb = 1;
+		else if (this_ray.rot == 90)
+			game->player.ub = 1;
+		else if (this_ray.rot == 180)
+			game->player.lb = 1;
+		else if (this_ray.rot == 270)
+			game->player.db = 1;
+	}
+	else
+	{
+		if (this_ray.rot == 0)
+			game->player.rb = 0;
+		else if (this_ray.rot == 90)
+			game->player.ub = 0;
+		else if (this_ray.rot == 180)
+			game->player.lb = 0;
+		else if (this_ray.rot == 270)
+			game->player.db = 0;
+	}
+}
+
+void	c3d_collider_check_all(t_game *game)
+{
+	t_ray	this_ray;
+	int		has_set;
+	int		i;
+	int		j;
 
 	this_ray.pos.x = game->player.pos.x;
 	this_ray.pos.y = game->player.pos.y;
@@ -31,80 +84,32 @@ void	c3d_collider_check_all(t_game *game)
 		has_set = 0;
 		j = 0;
 		while (game->walls[j])
-		{	
-			point = NULL;
-			point = ray_cast(game->walls[j], &this_ray);
-			if (point && game->walls[j]->type != DOOR)
-			{
-				pt_dist = -1;
-				pt_dist = get_raylength(this_ray, *point);
-				if (pt_dist != -1 && pt_dist < 15)
-				{
-					has_set = 1;
-				}
-			}
-			else if (point && game->walls[j]->type == DOOR)
-			{
-				if (game->walls[j]->is_active)
-				{
-					pt_dist = -1;
-					pt_dist = get_raylength(this_ray, *point);
-					if (pt_dist != -1 && pt_dist < 15)
-					{
-						has_set = 1;
-					}
-				}
-			}
-			else
-			{}
-			if (point)
-				free(point);
-			j++;
-		}
-		if (has_set)
-		{
-			if (this_ray.rot == 0)
-				game->player.rb = 1;
-			else if (this_ray.rot == 90)
-				game->player.ub = 1;
-			else if (this_ray.rot == 180)
-				game->player.lb = 1;
-			else if (this_ray.rot == 270)
-				game->player.db = 1;
-		}
-		else
-		{	
-			if (this_ray.rot == 0)
-				game->player.rb = 0;
-			else if (this_ray.rot == 90)
-				game->player.ub = 0;
-			else if (this_ray.rot == 180)
-				game->player.lb = 0;
-			else if (this_ray.rot == 270)
-				game->player.db = 0;
-		}
+			c3d_collider_check_all_do1(game->walls[j++], &this_ray, &has_set);
+		c3d_set_bumpers(game, this_ray, has_set);
 		i++;
-	}		
+	}
 }
 
-
+/*
+ * Unused feedback function that shows which colliders have 
+ * been triggered visually on the minimap.
 int	c3d_collider_feedback(t_game *game)
 {
-	t_point start;
-	t_point end;
-	t_vector unitVector;
-	double radius = 15;
-	double radians;
-	int i = 0;
+	t_point		start;
+	t_point		end;
+	double		radius;
+	double		radians;
+	int			i;
+
+	radius = 15;
+	i = 0;
 	while (i < 4)
 	{
 		radians = i * (M_PI / 2);
 		start.x = 150;
 		start.y = 150;
-		unitVector.x = cos(radians) * radius;
-		unitVector.y = sin(radians) * radius;
-		end.x = start.x + unitVector.x;
-		end.y = start.y + unitVector.y;
+		end.x = start.x + cos(radians) * radius;
+		end.y = start.y + sin(radians) * radius;
 		if (i == 0)
 		{
 			if (game->player.rb == 1)
@@ -133,14 +138,14 @@ int	c3d_collider_feedback(t_game *game)
 			else
 				line(game->img, start, end, 0xFFFF00);
 		}
-
 		i++;
 	}
 	return (0);
 }
+*/
 
 void	test_hitrays(t_game *game)
 {
 	c3d_collider_check_all(game);
-//	c3d_collider_feedback(game);
 }
+//	c3d_collider_feedback(game);

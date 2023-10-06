1cfe550310afcb56ef7c8de90f54ff00a5626ac0
Isaac Vanderwal <isaacvanderwal@hotmail.com>
2023-10-06 13:13:00 +1030

    norm: bounds.c

--- src/bounds.c
+++ src/bounds.c
@@ -6,18 +6,22 @@
 /*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:44:26 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/10/06 12:55:44 by ivanderw         ###   ########.fr       */
+/*   Updated: 2023/10/06 13:12:43 by ivanderw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bounds.h"
 
-void free_bounds(t_bound* head)
+void	free_bounds(t_bound *head)
 {
-    t_bound* current = head;
-    while (current != NULL) {
-        t_bound* temp = current;
-        current = current->next;
-        free(temp);
-    }
+	t_bound	*current;
+	t_bound	*temp;
+
+	current = head;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
 }

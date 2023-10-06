4752589b96b080c1ed5d7610050e515f69ed3d3a
Isaac Vanderwal <isaacvanderwal@hotmail.com>
2023-10-06 21:33:41 +1030

    norm: init_player.c

--- src/init_player.c
+++ src/init_player.c
@@ -6,15 +6,13 @@
 /*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 15:16:09 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/10/06 11:18:00 by ivanderw         ###   ########.fr       */
+/*   Updated: 2023/10/06 21:33:20 by ivanderw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "init_player.h"
 
-//game->player.rot = 0;
-
-int is_player_char(char c)
+int	is_player_char(char c)
 {
 	if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
 		return (1);
@@ -53,12 +51,11 @@ int	c3d_set_player_pos(t_game *game)
 			{
 				game->player.pos.y = (i * 64) + 32;
 				game->player.pos.x = (j * 64) + 32;
-				c3d_set_player_rot(game, game->raw[i][j]);	
-			}	
+				c3d_set_player_rot(game, game->raw[i][j]);
+			}
 			j++;
 		}
 		i++;
 	}
 	return (0);
 }
-

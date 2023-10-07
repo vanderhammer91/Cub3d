f3f137b07e86b7edf1f915e4869d35bb2143bed4
Isaac Vanderwal <isaacvanderwal@hotmail.com>
2023-10-07 16:32:17 +1030

    norm: fixed

--- inc/raycast.h
+++ inc/raycast.h
@@ -6,7 +6,7 @@
 /*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 15:12:21 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/10/07 16:27:21 by ivanderw         ###   ########.fr       */
+/*   Updated: 2023/10/07 16:32:01 by ivanderw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@
 t_point	*ray_cast(t_bound *current_wall, t_ray *this_ray);
 float	get_raylength(t_ray ray, t_point end);
 int		retrieve_colour(void *img, int x, int y);
-
-
+int		shade_pixel(int pixel_colour, float raylength);
 
 void	c3d_draw_projection(t_game *game, float raylength, int i, int ray_x,
 		int ray_y, t_bound *closest_wall);

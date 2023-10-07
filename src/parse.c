0ce111d9218c2456ac9de87ee508e2c45eb1991b
Louis Xu <louis.s.xu@gmail.com>
2023-10-07 16:37:53 +1030

    norm: Norm fixes

--- src/parse.c
+++ src/parse.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lxu <lxu@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 16:13:50 by lxu               #+#    #+#             */
+/*   Updated: 2023/10/07 16:37:28 by lxu              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parse.h"
 #include "raw_utils.h"
 
@@ -6,7 +18,8 @@
 // Map must be comprised of 0, 1, N, S, E, W
 // Must be closed/surronded by walls (flood fill)
 // Map content must be last, otherwise other lines can be beforehand
-// Except for the map, each type of information from an element can be separated by one or more spaces
+// Except for the map, each type of information from an element can be separated
+// by one or more spaces
 // spaces are valid and are up to you to handle
 
 t_parsed_data	*new_parsed_data(void)
@@ -91,8 +104,10 @@ void	delete_parsed_data(t_parsed_data *d)
 }
 
 
-//TODO decide what is a valid filename? apparently can contain any byte other than NUL or /
-// Ref: https://stackoverflow.com/questions/4814040/allowed-characters-in-filename
+//TODO decide what is a valid filename? apparently can contain any byte other
+// than NUL or /
+// Ref:
+// https://stackoverflow.com/questions/4814040/allowed-characters-in-filename
 size_t	valid_path_string_length(char *line)
 {
 	size_t	i;
@@ -278,9 +293,32 @@ size_t	arr_len(char **arr)
 	return (len);
 }
 
-t_char_and_rgb line_to_colour_data(char *line)
+t_char_and_rgb	arr_to_colour_data(char type, char **arr)
 {
 	t_char_and_rgb	res;
+
+	if (arr_len(arr) != 3)
+	{
+		res.c = '\0';
+		return (res);
+	}
+	res.r = ft_atoi(arr[0]);
+	res.g = ft_atoi(arr[1]);
+	res.b = ft_atoi(arr[2]);
+	if (res.r < 0 || res.r > 255 || \
+		res.g < 0 || res.g > 255 || res.b < 0 || res.b > 255)
+	{
+		res.c = '\0';
+		return (res);
+	}
+	res.c = type;
+	return (res);
+}
+
+t_char_and_rgb	line_to_colour_data(char *line)
+{
+	t_char_and_rgb	res;
+	char			type;
 	char			*trimmed_line;
 	char			**split_strs;
 
@@ -289,27 +327,13 @@ t_char_and_rgb line_to_colour_data(char *line)
 	if (!trimmed_line)
 		return (res);
 	if (ft_strncmp(trimmed_line, "F ", 2) == 0)
-		res.c = 'F';
+		type = 'F';
 	if (ft_strncmp(trimmed_line, "C ", 2) == 0)
-		res.c = 'C';
+		type = 'C';
 	split_strs = ft_split(&trimmed_line[2], ',');
 	free(trimmed_line);
-	if (arr_len(split_strs) != 3)
-	{
-		free_char_array(split_strs);
-		res.c = '\0';
-		return (res);
-	}
-	res.r = ft_atoi(split_strs[0]);
-	res.g = ft_atoi(split_strs[1]);
-	res.b = ft_atoi(split_strs[2]);
+	res = arr_to_colour_data(type, split_strs);
 	free_char_array(split_strs);
-	if (res.r < 0 || res.r > 255 || \
-		res.g < 0 || res.g > 255 || res.b < 0 || res.b > 255)
-	{
-		res.c = '\0';
-		return (res);
-	}
 	return (res);
 }
 
@@ -350,16 +374,10 @@ char	**ft_realloc(char **ptr, size_t old, size_t new)
 	size_t	i;
 
 	if (new < old)
-	{
-		free(ptr);
-		return (NULL);
-	}
+		return (free(ptr), NULL);
 	new_ptr = malloc(sizeof (*ptr) * new);
 	if (!new_ptr)
-	{
-		free (ptr);
-		return (NULL);
-	}
+		return (free(ptr), NULL);
 	i = 0;
 	while (ptr && ptr[i] && i < new)
 	{
@@ -390,7 +408,6 @@ int	is_valid_line_map(char *line)
 			line[i] == 'X' || line[i] == '2' || line[i] == '4' || \
 		line[i] == 'N' || line[i] == 'S' || line[i] == 'E' || line[i] == 'W')
 		{
-			// pass;
 		}
 		else
 		{
@@ -454,15 +471,10 @@ char	*extend_map_line(char *line, size_t new_width)
 	size_t	i;
 
 	if (new_width <= ft_strlen(line))
-	{
 		return (line);
-	}
 	new_line = malloc(sizeof (*line) * (new_width + 1));
 	if (!new_line)
-	{
-		free (line);
-		return (NULL);
-	}
+		return (free(line), NULL);
 	i = 0;
 	while (line[i])
 	{
@@ -505,31 +517,22 @@ int	map_has_only_one_start_location(t_parsed_data *d)
 
 	start_seen = 0;
 	if (!d->map)
-	{
 		return (0);
-	}
 	i = 0;
 	while (d->map[i])
 	{
 		j = 0;
 		while (d->map[i][j])
 		{
-			if (d->map[i][j] == 'N' || d->map[i][j] == 'E' || d->map[i][j] == 'W' || d->map[i][j] == 'S')
-			{
+			if (d->map[i][j] == 'N' || d->map[i][j] == 'E' || \
+				d->map[i][j] == 'W' || d->map[i][j] == 'S')
 				start_seen++;
-				if (start_seen > 1)
-				{
-					return (0);
-				}
-			}
 			j++;
 		}
 		i++;
 	}
 	if (start_seen == 1)
-	{
 		return (1);
-	}
 	return (0);
 }
 
@@ -542,20 +545,17 @@ t_coord	get_start_location(t_parsed_data *d)
 	res.x = -1;
 	res.y = -1;
 	if (!d->map)
-	{
 		return (res);
-	}
 	i = 0;
 	while (d->map[i])
 	{
 		j = 0;
 		while (d->map[i][j])
 		{
-			if (d->map[i][j] == 'N' || d->map[i][j] == 'E' || d->map[i][j] == 'W' || d->map[i][j] == 'S')
+			if (d->map[i][j] == 'N' || d->map[i][j] == 'E' || \
+				d->map[i][j] == 'W' || d->map[i][j] == 'S')
 			{
-				res.x = j;
-				res.y = i;
-				return (res);
+				return (res.x = j, res.y = i, res);
 			}
 			j++;
 		}
@@ -566,8 +566,9 @@ t_coord	get_start_location(t_parsed_data *d)
 
 size_t	arr_height(char **arr)
 {
-	size_t	height = 0;
-	
+	size_t	height;
+
+	height = 0;
 	while (arr[height])
 	{
 		height++;
@@ -585,32 +586,17 @@ char	**duplicate_map(char **map)
 	height = arr_height(map);
 	dup = malloc(sizeof (*map) * (height + 1));
 	if (!dup)
-	{
 		return (NULL);
-	}
 	dup[height] = NULL;
 	i = 0;
 	while (map[i])
 	{
 		line = ft_strdup(map[i]);
 		if (!line)
-		{
-			free_char_array(dup);
-			return (NULL);
-		}
+			return (free_char_array(dup), NULL);
 		dup[i] = line;
 		if (!dup[i])
-		{
-			free_char_array(dup);
-			// j = 0;
-			// while(j < i)
-			// {
-			// 	free(dup[j]);
-			// 	j++;
-			// }
-			// free(dup);
-			return (NULL);
-		}
+			return (free_char_array(dup), NULL);
 		i++;
 	}
 	return (dup);
@@ -619,18 +605,13 @@ char	**duplicate_map(char **map)
 int is_valid_by_flood_fill(char **map, int x, int y)
 {
 	if (x < 0 || y < 0)
-	{
 		return (0);
-	}
 	if (y >= (int)arr_height(map))
-	{
 		return (0);
-	}
 	if (x >= (int)ft_strlen(map[y]))
-	{
 		return (0);
-	}
-	if (map[y][x] == '0' || map[y][x] == 'N' || map[y][x] == 'S' || map[y][x] == 'W' || map[y][x] == 'E')
+	if (map[y][x] == '0' || map[y][x] == 'N' || map[y][x] == 'S' || \
+		map[y][x] == 'W' || map[y][x] == 'E')
 	{
 		map[y][x] = 'x';
 		if (is_valid_by_flood_fill(map, x - 1, y) == 0 || \
@@ -641,7 +622,6 @@ int is_valid_by_flood_fill(char **map, int x, int y)
 			return (0);
 		}
 	}
-	// else is in range, and is not a 0 char
 	return (1);
 }
 
@@ -650,7 +630,7 @@ int	is_enclosed_map(t_parsed_data *d)
 	char	**dup;
 	t_coord	start;
 	int		res;
-	
+
 	if (!d)
 	{
 		return (0);
@@ -666,6 +646,7 @@ int	is_enclosed_map(t_parsed_data *d)
 	return (res);
 }
 
+// other checks for validity other than just filled?
 int	parsed_data_is_valid(t_parsed_data *d)
 {
 	if (!d || \
@@ -683,7 +664,6 @@ int	parsed_data_is_valid(t_parsed_data *d)
 	{
 		return (0);
 	}
-	// other checks for validity other than just filled?
 	return (1);
 }
 
@@ -702,41 +682,6 @@ void	remove_trailing_new_line_char(char *line)
 	}
 	return ;
 }
-
-/*
-void	set_starting_coordinates_and_direction(t_parsed_data *d)
-{
-	t_coord	start_loc;
-
-	if (!d)
-	{
-		return ;
-	}
-	start_loc = get_start_location(d);
-	d->starting_x = start_loc.x;
-	d->starting_y = start_loc.y;
-	if (!d->map)
-	{
-		return ;
-	}
-	if (d->map[start_loc.y][start_loc.x] == 'N')
-	{
-		d->starting_dir = NORTH;
-	}
-	if (d->map[start_loc.y][start_loc.x] == 'E')
-	{
-		d->starting_dir = EAST;
-	}
-	if (d->map[start_loc.y][start_loc.x] == 'W')
-	{
-		d->starting_dir = WEST;
-	}
-	if (d->map[start_loc.y][start_loc.x] == 'S')
-	{
-		d->starting_dir = SOUTH;
-	}
-}
-*/
 
 int	is_file_name_valid(char *file_name)
 {
@@ -770,18 +715,21 @@ t_parsed_data	*file_to_data(char *file_name)
 
 	if (is_file_name_valid(file_name) == 0)
 	{
+		printf("file name invalid\n");
 		return (NULL);
 	}
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 	{
 		// error handle
+		printf("file open error\n");
 		return (NULL);
 	}
 	d = new_parsed_data();
 	if (!d)
 	{
 		close(fd);
+		printf("malloc error\n");
 		return (NULL);
 	}
 	while (1)
@@ -798,6 +746,7 @@ t_parsed_data	*file_to_data(char *file_name)
 				free(texture_data.str);
 				delete_parsed_data(d);
 				close(fd);
+				printf("duplicate texture data\n");
 				return (NULL);
 			}
 			poke_texture_data(d, texture_data);
@@ -806,11 +755,14 @@ t_parsed_data	*file_to_data(char *file_name)
 		{
 			// doing colour
 			colour_data = line_to_colour_data(line);
+			printf("line was <%s>\n", line);
 			free(line);
 			if (can_poke_colour_data(d, colour_data) == 0)
 			{
 				delete_parsed_data(d);
 				close(fd);
+				printf("duplicate colour data\n");
+				debug_print_parsed_data(d);
 				return (NULL);
 			}
 			poke_colour_data(d, colour_data);
@@ -846,6 +798,7 @@ t_parsed_data	*file_to_data(char *file_name)
 				if (!d->map)
 				{
 					delete_parsed_data(d);
+					printf("malloc/realloc failure\n");
 					return (NULL);
 				}
 			}
@@ -860,6 +813,7 @@ t_parsed_data	*file_to_data(char *file_name)
 			free(line);
 			delete_parsed_data(d);
 			close(fd);
+			printf("not a map line within map block\n");
 			return (NULL);
 		}
 		line = get_next_line(fd);
@@ -886,6 +840,7 @@ t_parsed_data	*file_to_data(char *file_name)
 	{
 		// managed to flood fill but found it wasn't valid
 		delete_parsed_data(d);
+		printf("not enclosed on flood fill\n");
 		return (NULL);
 	}
 	// managed to flood fill successfully (is enclosed)
@@ -893,17 +848,28 @@ t_parsed_data	*file_to_data(char *file_name)
 	if (parsed_data_is_valid(d) == 0)
 	{
 		delete_parsed_data(d);
+		printf("parsed data not all filled\n");
 		return (NULL);
 	}
 	// return
 	return (d);
 }
 
-// Debug print out data
-void	debug_print_parsed_data(t_parsed_data *d)
+void	debug_print_char_array(char **arr)
 {
 	size_t	i;
 
+	i = 0;
+	while (arr[i])
+	{
+		printf("  line %2ld: <%s>\n", i, arr[i]);
+		i++;
+	}
+}
+
+// Debug print out data
+void	debug_print_parsed_data(t_parsed_data *d)
+{
 	if (!d)
 	{
 		printf("(null)\n");
@@ -913,51 +879,16 @@ void	debug_print_parsed_data(t_parsed_data *d)
 	printf("south texture: %s\n", d->south_texture);
 	printf("west texture : %s\n", d->west_texture);
 	printf("east texture : %s\n", d->east_texture);
-	printf("ceiling (r, g, b): (%d, %d, %d)\n", d->ceiling_r, d->ceiling_g, d->ceiling_b);
-	printf("floor (r, g, b)  : (%d, %d, %d)\n", d->floor_r, d->floor_g, d->floor_b);
-//	printf("starting location (x, y) (col, row): (%d, %d)\n", d->starting_x, d->starting_y);
-//	printf("starting_direction: ");
-/*
-	if (d->starting_dir == UNSET)
-	{
-		printf("UNSET");
-	}
-	else if (d->starting_dir == NORTH)
-	{
-		printf("NORTH");
-	}
-	else if (d->starting_dir == EAST)
-	{
-		printf("EAST");
-	}
-	else if (d->starting_dir == WEST)
-	{
-		printf("WEST");
-	}
-	else if (d->starting_dir == SOUTH)
-	{
-		printf("SOUTH");
-	}
-	else
-	{
-		printf("UNKNOWN");
-	}
-	printf("\n");
-
-	*/
+	printf("ceiling (r, g, b): (%d, %d, %d)\n", \
+		d->ceiling_r, d->ceiling_g, d->ceiling_b);
+	printf("floor (r, g, b)  : (%d, %d, %d)\n", \
+		d->floor_r, d->floor_g, d->floor_b);
 	if (!d->map)
-	{
 		printf("map: (null)\n");
-	}
 	else
 	{
 		printf("map:\n");
-		i = 0;
-		while (d->map[i])
-		{
-			printf("  line %2ld: <%s>\n", i, d->map[i]);
-			i++;
-		}
+		debug_print_char_array(d->map);
 	}
 }
 

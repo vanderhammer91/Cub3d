beca76ee7f9bcb2bc76f1c234a530fe7b99c5726
Isaac Vanderwal <isaacvanderwal@hotmail.com>
2023-10-10 15:02:59 +1030

    rming ad, too inconsistent

--- src/raycast3.c
+++ src/raycast3.c
@@ -6,41 +6,11 @@
 /*   By: ivanderw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 20:20:54 by ivanderw          #+#    #+#             */
-/*   Updated: 2023/10/09 21:41:07 by ivanderw         ###   ########.fr       */
+/*   Updated: 2023/10/10 15:02:31 by ivanderw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycast.h"
-
-void	c3d_draw_ad_projection(t_game *game, t_projection *dp, t_param tp)
-{
-	dp->img_state = game->walls[game->cwi]->img_state;
-	dp->start_y = dp->y_offset - tp.raylength * 0.5;
-	dp->ty = 0;
-	dp->ty_step = 64.0 / (float)tp.raylength;
-	dp->tx = (int)(tp.ray_x) % 128;
-	dp->tx = 127 - dp->tx;
-	while (dp->y < tp.raylength)
-	{
-		dp->pixel_colour = retrieve_colour(game->t_lib.ad_texture, dp->tx * 2,
-				dp->ty + dp->img_state * 64);
-		dp->pixel_colour = shade_pixel(dp->pixel_colour, tp.raylength);
-		if (dp->pixel_colour != 0x000000)
-		{
-			dp->start.x = dp->x_offset + tp.i * dp->cw;
-			dp->start.y = dp->start_y + dp->y;
-			dp->end.x = dp->cw;
-			dp->end.y = 1;
-			rect(game->img, &dp->start, &dp->end, dp->pixel_colour);
-		}
-		dp->ty += dp->ty_step;
-		if (dp->ty >= 64)
-		{
-			dp->ty = 63;
-		}
-		dp->y++;
-	}
-}
 
 void	c3d_draw_door_projection_init(t_game *game, t_projection *dp,
 		t_param tp)
